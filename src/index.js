0328cd12d06c348a53960b458c771037863b0533
LeeJaeYun <ag502@naver.com>
2021-05-16 01:46:00 +0900

    basic

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import "./styles.css";
 const SignupForm = () => {
     const formik = useFormik({
         initialValues: {
+            firstName: "",
+            lastName: "",
             email: "",
         },
         onSubmit: values => {
@@ -14,6 +16,22 @@ const SignupForm = () => {
     });
     return (
         <form onSubmit={formik.handleSubmit}>
+            <label htmlFor="firstName">First Name</label>
+            <input
+                id="firstName"
+                name="firstName"
+                type="text"
+                onChange={formik.handleChange}
+                value={formik.values.firstName}
+            />
+            <label htmlFor="lastName">Last Name</label>
+            <input
+                id="lastName"
+                name="lastName"
+                type="text"
+                onChange={formik.handleChange}
+                value={formik.values.lastName}
+            />
             <label htmlFor="email">Email Address</label>
             <input
                 id="email"
